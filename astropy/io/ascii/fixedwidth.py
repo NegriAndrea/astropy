4c5c4170f796364837a990a4a968c5c16140e591
Andrea Negri <negri.andre@gmail.com>
2017-08-18 12:04:05 +0100

    added a line to do the numberign of columns

--- astropy/io/ascii/fixedwidth.py
+++ astropy/io/ascii/fixedwidth.py
@@ -258,7 +258,7 @@ class FixedWidthData(basic.BasicData):
             if self.header.start_line is not None:
                 col.width = max(col.width, len(col.info.name))
                 if self.header.unit_line is not None:
-                    col.width = max(col.width, len(str(col.unit)))
+                    col.width = max(col.width, len(str(col.unit)), len(self.cols)+2)
 
         widths = [col.width for col in self.cols]
 
@@ -267,6 +267,8 @@ class FixedWidthData(basic.BasicData):
                                             widths))
             if self.header.unit_line is not None:
                 lines.append(self.splitter.join(['' if col.unit is None else str(col.unit) for col in self.cols], widths))
+                lines.append(self.splitter.join(['('+str(i)+')' for i in
+                    range(len(self.cols)+1)], widths))
 
 
         if self.header.position_line is not None:
